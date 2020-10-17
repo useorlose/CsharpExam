51baaade7ebea2668101ef8ab1e7b1ea8bbee677
Jyothsna <71509444+useorlose@users.noreply.github.com>
2020-10-17 14:35:00 +0530

    Add files via upload

---
+++ Question1/ArrayDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class ArrayDisplay
+    {
+        static void DiaplayArray(string arr)
+        {
+            Console.Write("Forward String: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+            Console.Write("Backward String: ");
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            string str;
+            try
+            {
+                Console.WriteLine("Enter the String");
+                str = Console.ReadLine();
+                DiaplayArray(str);
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
